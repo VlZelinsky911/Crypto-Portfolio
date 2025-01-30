9f381e264aa28ba2dc6112555953b26640e68f22
VLZelinsky911 <zelinskyvlad991@gmail.com>
2025-01-30 13:38:56 +0200

    finishing

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,5 +1,7 @@
 import { Layout, Typography } from "antd";
 import { useCrypto } from "../../context/crypto-context";
+import PortfolioChart from "./PortfolioChart";
+import AssetsTable from "../AssetsTable";
 
 const contentStyle = {
   textAlign: "center",
@@ -12,8 +14,9 @@ const contentStyle = {
 export default function AppContent() {
   const { assets, crypto } = useCrypto();
 
-	const cryptoPriceMap = crypto.reduce((acc, c) =>{
+	const cryptoPriceMap = crypto.reduce((acc, c) => {
 	acc[c.id] = c.price
+	return acc
 	},{})
 
   return (
@@ -21,11 +24,15 @@ export default function AppContent() {
       <Typography.Title level={3} style={{ textAlign: "left", color: "#fff" }}>
         Portfolio:{" "}
         {assets
-          .map((asset) => {return asset.amount * cryptoPriceMap})
+          .map((asset) => {
+						return asset.amount * cryptoPriceMap[asset.id]
+					})
           .reduce((acc, c) => acc + c, 0)
           .toFixed(2)}
         $
       </Typography.Title>
+			<PortfolioChart/>
+			<AssetsTable/>
     </Layout.Content>
   );
 }
